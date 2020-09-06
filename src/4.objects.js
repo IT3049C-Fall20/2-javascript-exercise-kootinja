8587cdba787956fff0397a170ce099fda90175f4
Jeanette <jkootinsanwu@gmail.com>
2020-09-06 16:31:08 -0400

    finished objects tests

--- src/4.objects.js
+++ src/4.objects.js
@@ -9,7 +9,15 @@
  *      * must use the `class_of` property in the introduction
  */
 const personObject = {
-
+  first_name: `Jeanette`,
+  last_name: `Kootin-Sanwu`,
+  class_of: `2022`,
+  full_name: function () {
+    return this.first_name + ` ` + this.last_name;
+  },
+  introduction: function () {
+    return `Hello everyone, my name is ` + this.full_name() + `I am apart of the class of  ` + this.class_of;
+  }
 };
 
 module.exports = {
